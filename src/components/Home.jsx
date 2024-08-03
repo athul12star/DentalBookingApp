d268e73e22edeaa6409aaf1b8be8070c3b2dff3e
athul12star <athul12star@gmail.com>
2024-08-03 17:35:38 -0400

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Our Dental Booking App</h1>
+      <p>Book your appointments with ease!</p>
+    </div>
+  );
+}
+
+export default Home;
